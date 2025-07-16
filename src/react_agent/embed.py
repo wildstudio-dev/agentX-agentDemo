e42947e8c94bd0dce5498109a113aed3e9fc35a2
Preslav Hadzhitsanev <p.hadzhitsanev@nolimit.software>
2025-07-16 16:16:59 +0300

    reduce chunk size and use open ai

--- src/react_agent/embed.py
+++ src/react_agent/embed.py
@@ -1,33 +1,33 @@
 import logging
-from google import genai
+# from google import genai
 from google.genai import types
 
-client = genai.Client()
+# client = genai.Client()
 
-# from openai import AsyncOpenAI
-# client = AsyncOpenAI()
+from openai import AsyncOpenAI
+client = AsyncOpenAI()
 
-def embed_texts(texts: list[str]) -> list[list[float]]:
+async def embed_texts(texts: list[str]) -> list[list[float]]:
     """Custom embedding function that must:
     1. Be async
     2. Accept a list of strings
     3. Return a list of float arrays (embeddings)
     """
     logging.info("Embedding texts len: %s", len(texts))
-    # response = await client.embeddings.create(
-    #     model="text-embedding-3-small",
-    #     input=texts
-    # )
-    # return [e.embedding for e in response.data]
-
-    response = client.models.embed_content(
-        model="gemini-embedding-exp-03-07",
-        contents=texts,
-        config=types.EmbedContentConfig(
-            task_type="SEMANTIC_SIMILARITY",
-            output_dimensionality=1536
-        )
+    response = await client.embeddings.create(
+        model="text-embedding-3-small",
+        input=texts
     )
-    print(response)
+    return [e.embedding for e in response.data]
+
+    # response = client.models.embed_content(
+    #     model="gemini-embedding-exp-03-07",
+    #     contents=texts,
+    #     config=types.EmbedContentConfig(
+    #         task_type="SEMANTIC_SIMILARITY",
+    #         output_dimensionality=1536
+    #     )
+    # )
+    # print(response)
     # print(len(response)
-    return [e.values for e in response.embeddings]
+    # return [e.values for e in response.embeddings]
